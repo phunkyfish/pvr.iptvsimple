f459df675cd5d8057199fd1d94c644b126a17102
Ross Nicholson <phunkyfish@gmail.com>
2019-08-15 19:51:46 -0700

    Refactoring - EpgGenre

---
+++ src/iptvsimple/data/EpgGenre.cpp
@@ -0,0 +1,12 @@
+#include "EpgGenre.h"
+
+#include "../Settings.h"
+#include "inttypes.h"
+#include "p8-platform/util/StringUtils.h"
+#include "util/XMLUtils.h"
+
+#include <regex>
+
+using namespace iptvsimple;
+using namespace iptvsimple::data;
+using namespace iptvsimple::utilities;
